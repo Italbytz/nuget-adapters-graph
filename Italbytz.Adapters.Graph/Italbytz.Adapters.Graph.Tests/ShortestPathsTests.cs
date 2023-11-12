b84ee6a6cd65b3c46b29b3f9172eff1f764fa079
nunkesser <robin.nunkesser@hshl.de>
2023-11-12 20:17:08 +0100

    Example graph

--- Italbytz.Adapters.Graph/Italbytz.Adapters.Graph.Tests/ShortestPathsTests.cs
+++ Italbytz.Adapters.Graph/Italbytz.Adapters.Graph.Tests/ShortestPathsTests.cs
@@ -30,58 +30,10 @@ namespace Italbytz.Adapters.Graph.Tests
         [Test]
         public void TestRomania()
         {
-
-            var vertex0 = "Arad";
-            var vertex1 = "Timisoara";
-            var vertex2 = "Zerind";
-            var vertex3 = "Oradea";
-            var vertex4 = "Lugoj";
-            var vertex5 = "Mehadia";
-            var vertex6 = "Drobeta";
-            var vertex7 = "Sibiu";
-            var vertex8 = "Rimnicu Vilcea";
-            var vertex9 = "Craiova";
-            var vertex10 = "Fagaras";
-            var vertex11 = "Pitesti";
-            var vertex12 = "Giurgiu";
-            var vertex13 = "Bukarest";
-            var vertex14 = "Urziceni";
-            var vertex15 = "Neamt";
-            var vertex16 = "Iasi";
-            var vertex17 = "Vaslui";
-            var vertex18 = "Hirsova";
-            var vertex19 = "Eforie";
-
-            var vertices = new string[] { vertex0, vertex1, vertex2, vertex3, vertex4, vertex5, vertex6, vertex7, vertex8, vertex9, vertex10, vertex11, vertex12, vertex13, vertex14, vertex15, vertex16, vertex17, vertex18, vertex19 };
-            var edges = new List<ITaggedEdge<string, double>>
-            {
-                new TaggedEdge<string, double>(vertex0, vertex1, 118.0),
-                new TaggedEdge<string, double>(vertex0, vertex2, 75.0),
-                new TaggedEdge<string, double>(vertex0, vertex7, 140.0),
-                new TaggedEdge<string, double>(vertex1, vertex4, 111.0),
-                new TaggedEdge<string, double>(vertex2, vertex3, 71.0),
-                new TaggedEdge<string, double>(vertex3, vertex7, 151.0),
-                new TaggedEdge<string, double>(vertex4, vertex5, 70.0),
-                new TaggedEdge<string, double>(vertex5, vertex6, 75.0),
-                new TaggedEdge<string, double>(vertex6, vertex9, 120.0),
-                new TaggedEdge<string, double>(vertex7, vertex8, 80.0),
-                new TaggedEdge<string, double>(vertex7, vertex10, 99.0),
-                new TaggedEdge<string, double>(vertex8, vertex9, 146.0),
-                new TaggedEdge<string, double>(vertex8, vertex11, 97.0),
-                new TaggedEdge<string, double>(vertex9, vertex11, 138.0),
-                new TaggedEdge<string, double>(vertex10, vertex13, 211.0),
-                new TaggedEdge<string, double>(vertex11, vertex13, 101.0),
-                new TaggedEdge<string, double>(vertex12, vertex13, 90.0),
-                new TaggedEdge<string, double>(vertex13, vertex14, 85.0),
-                new TaggedEdge<string, double>(vertex14, vertex17, 142.0),
-                new TaggedEdge<string, double>(vertex14, vertex18, 98.0),
-                new TaggedEdge<string, double>(vertex15, vertex16, 87.0),
-                new TaggedEdge<string, double>(vertex16, vertex17, 92.0),
-                new TaggedEdge<string, double>(vertex18, vertex19, 86.0)
-            };
-            var graph = new UndirectedGraph<string, ITaggedEdge<string, double>>() { Edges = edges };
+            var graph = Graphs.Instance.AIMARomania;
+            var vertices = graph.ToQuikGraph().Vertices.ToArray();
             var parameters = new ShortestPathsParameters(vertices, graph);
-            solver = new ShortestPathsSolver(vertex0);
+            solver = new ShortestPathsSolver("Arad");
 
             var solution = solver.Solve(parameters);
             foreach (var path in solution.Paths)
---
+++ Italbytz.Adapters.Graph/Italbytz.Adapters.Graph/Graphs.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Italbytz.Ports.Graph;
+using System.Collections.Generic;
+
+namespace Italbytz.Adapters.Graph
+{
+    public sealed class Graphs
+    {
+        private static readonly Lazy<Graphs> lazy =
+            new(() => new Graphs());
+
+        public static Graphs Instance { get { return lazy.Value; } }
+
+        public UndirectedGraph<string, ITaggedEdge<string, double>> AIMARomania { get; }
+
+        private Graphs()
+        {
+            AIMARomania = buildAIMARomania();
+        }
+
+        private UndirectedGraph<string, ITaggedEdge<string, double>> buildAIMARomania()
+        {
+            var vertex0 = "Arad";
+            var vertex1 = "Timisoara";
+            var vertex2 = "Zerind";
+            var vertex3 = "Oradea";
+            var vertex4 = "Lugoj";
+            var vertex5 = "Mehadia";
+            var vertex6 = "Drobeta";
+            var vertex7 = "Sibiu";
+            var vertex8 = "Rimnicu Vilcea";
+            var vertex9 = "Craiova";
+            var vertex10 = "Fagaras";
+            var vertex11 = "Pitesti";
+            var vertex12 = "Giurgiu";
+            var vertex13 = "Bukarest";
+            var vertex14 = "Urziceni";
+            var vertex15 = "Neamt";
+            var vertex16 = "Iasi";
+            var vertex17 = "Vaslui";
+            var vertex18 = "Hirsova";
+            var vertex19 = "Eforie";
+
+            var edges = new List<ITaggedEdge<string, double>>
+            {
+                new TaggedEdge<string, double>(vertex0, vertex1, 118.0),
+                new TaggedEdge<string, double>(vertex0, vertex2, 75.0),
+                new TaggedEdge<string, double>(vertex0, vertex7, 140.0),
+                new TaggedEdge<string, double>(vertex1, vertex4, 111.0),
+                new TaggedEdge<string, double>(vertex2, vertex3, 71.0),
+                new TaggedEdge<string, double>(vertex3, vertex7, 151.0),
+                new TaggedEdge<string, double>(vertex4, vertex5, 70.0),
+                new TaggedEdge<string, double>(vertex5, vertex6, 75.0),
+                new TaggedEdge<string, double>(vertex6, vertex9, 120.0),
+                new TaggedEdge<string, double>(vertex7, vertex8, 80.0),
+                new TaggedEdge<string, double>(vertex7, vertex10, 99.0),
+                new TaggedEdge<string, double>(vertex8, vertex9, 146.0),
+                new TaggedEdge<string, double>(vertex8, vertex11, 97.0),
+                new TaggedEdge<string, double>(vertex9, vertex11, 138.0),
+                new TaggedEdge<string, double>(vertex10, vertex13, 211.0),
+                new TaggedEdge<string, double>(vertex11, vertex13, 101.0),
+                new TaggedEdge<string, double>(vertex12, vertex13, 90.0),
+                new TaggedEdge<string, double>(vertex13, vertex14, 85.0),
+                new TaggedEdge<string, double>(vertex14, vertex17, 142.0),
+                new TaggedEdge<string, double>(vertex14, vertex18, 98.0),
+                new TaggedEdge<string, double>(vertex15, vertex16, 87.0),
+                new TaggedEdge<string, double>(vertex16, vertex17, 92.0),
+                new TaggedEdge<string, double>(vertex18, vertex19, 86.0)
+            };
+            return new UndirectedGraph<string, ITaggedEdge<string, double>>() { Edges = edges };
+        }
+    }
+}
+
