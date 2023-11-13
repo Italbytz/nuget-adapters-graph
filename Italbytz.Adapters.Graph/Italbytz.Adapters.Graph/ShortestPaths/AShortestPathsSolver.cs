40a0993b95311a8ab5fb2a0c92bd954bf2fde63e
nunkesser <robin.nunkesser@hshl.de>
2023-11-13 11:46:24 +0100

    Abstract solver

---
+++ Italbytz.Adapters.Graph/Italbytz.Adapters.Graph/ShortestPaths/AShortestPathsSolver.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using Italbytz.Ports.Graph;
+using QuikGraph;
+using QuikGraph.Algorithms.Observers;
+using QuikGraph.Algorithms.ShortestPath;
+
+namespace Italbytz.Adapters.Graph
+{
+    public abstract class AShortestPathsSolver : IShortestPathsSolver
+    {
+        protected string rootVertex;
+        protected readonly Dictionary<string, double> verticesCost = new();
+        protected readonly Dictionary<string, bool> expandedVertices = new();
+        protected readonly Dictionary<(string, string, double), bool> examinedEdges = new();
+        protected readonly Dictionary<(string, string, double), bool> treeEdges = new();
+
+        protected Ports.Graph.IUndirectedGraph<string, ITaggedEdge<string, double>>? originalGraph;
+        protected int file = 0;
+
+        public AShortestPathsSolver() : this("A") { }
+
+        public AShortestPathsSolver(string rootVertex)
+        {
+            this.rootVertex = rootVertex;
+        }
+
+        public IShortestPathsSolution Solve(IShortestPathsParameters parameters)
+        {
+            originalGraph = parameters.Graph;
+            var graph = originalGraph.ToQuikGraph();
+
+            var bidirectionalGraph = graph.ToBidirectionalGraph();
+            var algorithm = GetAlgorithm(bidirectionalGraph);
+
+            InitializeVerticesDictionaries(graph.Vertices);
+            InitializeEdgesDictionaries(graph.Edges);
+            SaveGraph();
+
+            if (algorithm is DijkstraShortestPathAlgorithm<string, QuikGraph.TaggedEdge<string, double>>)
+            {
+                ((DijkstraShortestPathAlgorithm<string, QuikGraph.TaggedEdge<string, double>>)algorithm).ExamineEdge += new EdgeAction<string, QuikGraph.TaggedEdge<string, double>>(ExamineEdgeHandler);
+            }
+
+            algorithm.TreeEdge += new EdgeAction<string, QuikGraph.TaggedEdge<string, double>>(TreeEdgeHandler);
+            var predecessorRecorder = new VertexPredecessorRecorderObserver<string, QuikGraph.TaggedEdge<string, double>>();
+            using (predecessorRecorder.Attach(algorithm))
+            {
+                algorithm.Compute(rootVertex);
+            }
+
+            var predecessors = predecessorRecorder.VerticesPredecessors;
+            TryFunc<string, IEnumerable<QuikGraph.TaggedEdge<string, double>>>
+                tryGetPaths = (string vertex, out IEnumerable<QuikGraph.TaggedEdge<string, double>> edges) => predecessors.TryGetPath(vertex, out edges);
+
+            var paths = new List<string>();
+            foreach (var vertex in parameters.Vertices)
+            {
+                if (vertex != rootVertex && tryGetPaths(vertex, out IEnumerable<QuikGraph.TaggedEdge<string, double>> path))
+                {
+                    var pathString = rootVertex;
+                    var cost = 0;
+                    var lastVertex = rootVertex;
+                    foreach (var edge in path)
+                    {
+                        cost += (int)edge.Tag;
+                        lastVertex = edge.GetOtherVertex(lastVertex);
+                        pathString += $" -> {lastVertex}";
+                    }
+                    pathString += $" ({cost})";
+                    paths.Add(pathString);
+                }
+            }
+
+            return new ShortestPathsSolution
+            {
+                Paths = paths
+            };
+        }
+
+        protected abstract ShortestPathAlgorithmBase<string, QuikGraph.TaggedEdge<string, double>, IVertexListGraph<string, QuikGraph.TaggedEdge<string, double>>> GetAlgorithm(BidirectionalGraph<string, QuikGraph.TaggedEdge<string, double>> graph);
+
+        protected void SaveGraph()
+        {
+            ((UndirectedGraph<string, ITaggedEdge<string, double>>)originalGraph).ToGraphviz(false, expandedVertices, examinedEdges, treeEdges, $"dijkstra_{rootVertex}_{file}.dot");
+            file++;
+        }
+
+        protected void TreeEdgeHandler(QuikGraph.TaggedEdge<string, double> edge)
+        {
+            treeEdges[(edge.Source, edge.Target, edge.Tag)] = true;
+            SaveGraph();
+            //Console.WriteLine($"Handling edge {edge.Source} -> {edge.Target}");
+        }
+
+        protected void ExamineEdgeHandler(QuikGraph.TaggedEdge<string, double> edge)
+        {
+            var pathCost = verticesCost[edge.Source] + edge.Tag;
+            if (!expandedVertices[edge.Source])
+            {
+                expandedVertices[edge.Source] = true;
+                //Console.WriteLine($"Expanding {edge.Source} with current cost {verticesCost[edge.Source]}");
+                SaveGraph();
+            }
+            examinedEdges[(edge.Source, edge.Target, edge.Tag)] = true;
+            SaveGraph();
+            //Console.WriteLine($"Examining {edge.Source} -> {edge.Target}: {edge.Tag}");
+            if (verticesCost[edge.Target] == double.MaxValue)
+            {
+                verticesCost[edge.Target] = pathCost;
+            }
+            else
+            {
+                if (pathCost < verticesCost[edge.Target])
+                {
+                    verticesCost[edge.Target] = pathCost;
+                }
+            }
+        }
+
+        protected void InitializeVerticesDictionaries(IEnumerable<string> vertices)
+        {
+            foreach (var vertex in vertices)
+            {
+                expandedVertices[vertex] = false;
+                verticesCost[vertex] = double.MaxValue;
+            }
+            verticesCost[rootVertex] = 0.0;
+        }
+
+        protected void InitializeEdgesDictionaries(IEnumerable<QuikGraph.TaggedEdge<string, double>> edges)
+        {
+            foreach (var edge in edges)
+            {
+                examinedEdges[(edge.Source, edge.Target, edge.Tag)] = false;
+                treeEdges[(edge.Source, edge.Target, edge.Tag)] = false;
+            }
+        }
+
+    }
+}
+
--- Italbytz.Adapters.Graph/Italbytz.Adapters.Graph/ShortestPaths/ShortestPathsSolver.cs
+++ Italbytz.Adapters.Graph/Italbytz.Adapters.Graph/ShortestPaths/ShortestPathsSolver.cs
@@ -16,152 +16,22 @@ using QuikGraph.Collections;
 
 namespace Italbytz.Adapters.Graph
 {
-    public class ShortestPathsSolver : IShortestPathsSolver
+    public class ShortestPathsSolver : AShortestPathsSolver
     {
-        private readonly string rootVertex;
-        private Ports.Graph.IUndirectedGraph<string, ITaggedEdge<string, double>> originalGraph;
-        int file = 0;
-        private readonly Dictionary<string, double> verticesCost = new();
-        private readonly Dictionary<string, bool> expandedVertices = new();
-        private readonly Dictionary<(string, string, double), bool> examinedEdges = new();
-        private readonly Dictionary<(string, string, double), bool> treeEdges = new();
-
-        public ShortestPathsSolver() : this("A") { }
-
-        public ShortestPathsSolver(string rootVertex)
+        public ShortestPathsSolver() : base()
         {
-            this.rootVertex = rootVertex;
+
         }
 
-        private void ExamineEdgeHandler(QuikGraph.TaggedEdge<string, double> edge)
+        public ShortestPathsSolver(string rootVertex) : base(rootVertex)
         {
-            var pathCost = verticesCost[edge.Source] + edge.Tag;
-            if (!expandedVertices[edge.Source])
-            {
-                expandedVertices[edge.Source] = true;
-                //Console.WriteLine($"Expanding {edge.Source} with current cost {verticesCost[edge.Source]}");
-                SaveGraph();
-            }
-            examinedEdges[(edge.Source, edge.Target, edge.Tag)] = true;
-            SaveGraph();
-            //Console.WriteLine($"Examining {edge.Source} -> {edge.Target}: {edge.Tag}");
-            if (verticesCost[edge.Target] == double.MaxValue)
-            {
-                verticesCost[edge.Target] = pathCost;
-            }
-            else
-            {
-                if (pathCost < verticesCost[edge.Target])
-                {
-                    verticesCost[edge.Target] = pathCost;
-                }
-            }
+
         }
 
-        private void SaveGraph()
+        protected override ShortestPathAlgorithmBase<string, QuikGraph.TaggedEdge<string, double>, IVertexListGraph<string, QuikGraph.TaggedEdge<string, double>>> GetAlgorithm(BidirectionalGraph<string, QuikGraph.TaggedEdge<string, double>> graph)
         {
-            ((UndirectedGraph<string, ITaggedEdge<string, double>>)originalGraph).ToGraphviz(false, expandedVertices, examinedEdges, treeEdges, $"dijkstra_{rootVertex}_{file}.dot");
-            file++;
+            return new DijkstraShortestPathAlgorithm<string, QuikGraph.TaggedEdge<string, double>>(graph, ((edge) => edge.Tag));
         }
 
-        public IShortestPathsSolution Solve(IShortestPathsParameters parameters)
-        {
-            originalGraph = parameters.Graph;
-            var graph = originalGraph.ToQuikGraph();
-
-            var bidirectionalGraph = graph.ToBidirectionalGraph();
-            var dijkstra = new DijkstraShortestPathAlgorithm<string, QuikGraph.TaggedEdge<string, double>>(bidirectionalGraph, ((edge) => edge.Tag));
-
-            InitializeVerticesDictionaries(graph.Vertices);
-            InitializeEdgesDictionaries(graph.Edges);
-            SaveGraph();
-
-            dijkstra.ExamineEdge += new EdgeAction<string, QuikGraph.TaggedEdge<string, double>>(ExamineEdgeHandler);
-            dijkstra.TreeEdge += new EdgeAction<string, QuikGraph.TaggedEdge<string, double>>(TreeEdgeHandler);
-            dijkstra.DiscoverVertex += new VertexAction<string>(DiscoverVertexHandler);
-            dijkstra.StartVertex += new VertexAction<string>(StartVertexHandler);
-            dijkstra.InitializeVertex += new VertexAction<string>(InitializeVertexHandler);
-            dijkstra.FinishVertex += new VertexAction<string>(FinishVertexHandler);
-            var predecessorRecorder = new VertexPredecessorRecorderObserver<string, QuikGraph.TaggedEdge<string, double>>();
-            using (predecessorRecorder.Attach(dijkstra))
-            {
-                dijkstra.Compute(rootVertex);
-            }
-
-            var predecessors = predecessorRecorder.VerticesPredecessors;
-            TryFunc<string, IEnumerable<QuikGraph.TaggedEdge<string, double>>>
-                tryGetPaths = (string vertex, out IEnumerable<QuikGraph.TaggedEdge<string, double>> edges) => predecessors.TryGetPath(vertex, out edges);
-
-            var paths = new List<string>();
-            foreach (var vertex in parameters.Vertices)
-            {
-                if (vertex != rootVertex && tryGetPaths(vertex, out IEnumerable<QuikGraph.TaggedEdge<string, double>> path))
-                {
-                    var pathString = rootVertex;
-                    var cost = 0;
-                    var lastVertex = rootVertex;
-                    foreach (var edge in path)
-                    {
-                        cost += (int)edge.Tag;
-                        lastVertex = edge.GetOtherVertex(lastVertex);
-                        pathString += $" -> {lastVertex}";
-                    }
-                    pathString += $" ({cost})";
-                    paths.Add(pathString);
-                }
-            }
-
-            return new ShortestPathsSolution
-            {
-                Paths = paths
-            };
-        }
-
-        private void TreeEdgeHandler(QuikGraph.TaggedEdge<string, double> edge)
-        {
-            treeEdges[(edge.Source, edge.Target, edge.Tag)] = true;
-            SaveGraph();
-            //Console.WriteLine($"Handling edge {edge.Source} -> {edge.Target}");
-        }
-
-        private void FinishVertexHandler(string vertex)
-        {
-            Console.WriteLine();
-            //Console.WriteLine($"Finishing vertex {vertex}");
-        }
-
-        private void InitializeVertexHandler(string vertex)
-        {
-            //Console.WriteLine($"Initializing vertex {vertex}");
-        }
-
-        private void StartVertexHandler(string vertex)
-        {
-            //Console.WriteLine($"Starting vertex {vertex}");
-        }
-
-        private void DiscoverVertexHandler(string vertex)
-        {
-            //Console.WriteLine($"Discovering vertex {vertex}");            
-        }
-
-        private void InitializeVerticesDictionaries(IEnumerable<string> vertices)
-        {
-            foreach (var vertex in vertices)
-            {
-                expandedVertices[vertex] = false;
-                verticesCost[vertex] = double.MaxValue;
-            }
-            verticesCost[rootVertex] = 0.0;
-        }
-
-        private void InitializeEdgesDictionaries(IEnumerable<QuikGraph.TaggedEdge<string, double>> edges)
-        {
-            foreach (var edge in edges)
-            {
-                examinedEdges[(edge.Source, edge.Target, edge.Tag)] = false;
-                treeEdges[(edge.Source, edge.Target, edge.Tag)] = false;
-            }
-        }
     }
 }
